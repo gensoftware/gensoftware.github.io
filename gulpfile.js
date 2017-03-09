79ba5fa5e15da11421c151fe8b9fc127591b9e35
Paula Grangeiro <contato@paulagrangeiro.com.br>
2017-03-09 02:40:39 -0300

    Arrange assets

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,10 @@ var banner = ['/*!\n',
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/new-age.less')
+    return gulp.src('assets/less/new-age.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -29,10 +29,10 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/new-age.css')
+    return gulp.src('assets/css/new-age.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -40,26 +40,26 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/new-age.js')
+    return gulp.src('assets/js/new-age.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('assets/js'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
-// Copy vendor libraries from /node_modules into /vendor
+// Copy assets libraries from /node_modules into /assets
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('assets/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('assets/jquery'))
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor/simple-line-icons'))
+        .pipe(gulp.dest('assets/simple-line-icons'))
 
 
     gulp.src([
@@ -70,7 +70,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('assets/font-awesome'))
 })
 
 // Run everything
@@ -87,10 +87,10 @@ gulp.task('browserSync', function() {
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('assets/less/*.less', ['less']);
+    gulp.watch('assets/css/*.css', ['minify-css']);
+    gulp.watch('assets/js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('assets/js/**/*.js', browserSync.reload);
 });
